b239fffc8055df4933ab9d4d2cc8921f4e1bf600
Ruikun <liruikun@outlook.com>
2020-04-28 17:36:11 +0200

    webcams

--- src/webcamList/webcams.py
+++ src/webcamList/webcams.py
@@ -1,8 +1,8 @@
 webcams = dict()
 webcams['london'] = 'https://www.earthcam.com/world/england/london/abbeyroad/?cam=abbeyroad_uk'
-webcams['new york'] = 'https://www.earthcam.com/usa/newyork/timessquare/?cam=tsrobo1'
+webcams['NYC'] = 'https://www.earthcam.com/usa/newyork/timessquare/?cam=tsrobo1'
 webcams['dublin'] = 'https://www.earthcam.com/world/ireland/dublin/?cam=templebar'
 webcams['prague'] = 'https://www.earthcam.com/world/czechrepublic/prague/?cam=grandhotel_str'
 webcams['elburg'] = 'https://www.youtube.com/watch?v=vPxAgn3EtDQ'
-webcams['dublinpub'] = 'https://www.earthcam.com/world/ireland/dublin/?cam=dublinpub'
+# webcams['dublinpub'] = 'https://www.earthcam.com/world/ireland/dublin/?cam=dublinpub'
 webcams['bruges'] = 'https://www.skylinewebcams.com/de/webcam/belgique/flandres/bruges/bruges.html'
