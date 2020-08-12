68d6ad9e2e03ea719b349abd6cc8167afe1d3ea4
Ruikun <liruikun@outlook.com>
2020-08-12 16:51:22 +0200

    - new webcams

--- src/webcamList/webcams.py
+++ src/webcamList/webcams.py
@@ -8,7 +8,7 @@ webcams['NYC_1'] = 'https://www.youtube.com/watch?v=srlpC5tmhYs' # unavailiable 
 webcams['NYC_2'] = 'https://www.earthcam.com/usa/newyork/fifthave/?cam=nyc5th_str'
 webcams['NYC_3'] = 'https://www.earthcam.com/usa/newyork/wallstreet/chargingbull/?cam=chargingbull_hd'
 webcams['Dublin'] = 'https://www.earthcam.com/world/ireland/dublin/?cam=templebar'
-webcams['Dublin_1'] = 'https://www.skylinewebcams.com/de/webcam/ireland/leinster/dublin/dublin-o-connell-street.html'
+webcams['Dublin_1'] = 'https://www.skylinewebcams.com/de/webcam/ireland/leinster/dublin/dublin-o-connell-street.html' #un
 webcams['Dublin_2'] = 'https://www.earthcam.com/world/ireland/dublin/?cam=dublinpub'
 webcams['LA'] = 'https://www.earthcam.com/usa/california/losangeles/hollywoodstars/?cam=hollywoodstars'
 webcams['LA_1'] = 'https://www.earthcam.com/usa/california/losangeles/hollywoodblvd/?cam=hollywoodblvd'
@@ -21,7 +21,10 @@ webcams['Venice_1'] = 'https://www.skylinewebcams.com/zh/webcam/italia/veneto/ve
 webcams['Venice_2'] = 'https://www.skylinewebcams.com/zh/webcam/italia/veneto/venezia/riva-schiavoni.html'
 webcams['Amsterdam'] = 'https://www.youtube.com/watch?v=hBnhSxlL9So'
 webcams['Rome'] = 'https://www.skylinewebcams.com/de/webcam/italia/lazio/roma/piazza-di-spagna.html'
-webcams['Rome_1'] = 'https://www.whatsupcams.com/en/webcams/italy/latium/rome/webcam-rome-navona-square/'
+# webcams['Rome_1'] = 'https://www.whatsupcams.com/en/webcams/italy/latium/rome/webcam-rome-navona-square/'#un
+webcams['Rome_2'] = 'https://www.skylinewebcams.com/it/webcam/italia/lazio/roma/fontana-di-trevi.html'
+webcams['Rome_3'] = 'https://www.skylinewebcams.com/it/webcam/italia/lazio/roma/pantheon.html'
+webcams['Rome_4'] = 'https://www.skylinewebcams.com/it/webcam/italia/lazio/roma/campo-de-fiori.html'
 webcams['Jesolo'] = 'https://www.youtube.com/watch?v=2Xjog3Xs-P0&feature=emb_logo'
 webcams['Jesolo_1'] = 'https://www.youtube.com/watch?v=zE_yzuxFiMo&feature=emb_logo'
 webcams['Miami'] = 'https://www.earthcam.com/usa/florida/miami/?cam=miami'
